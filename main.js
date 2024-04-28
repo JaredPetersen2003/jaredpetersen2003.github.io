89288baf69896fbc0ae84b439b02e0a406cbb371
JaredPetersen2003 <jared.petersen9@gmail.com>
2024-04-29 00:29:58 +0200

    fixed scroll

--- main.js
+++ main.js
@@ -1,17 +1,5 @@
 import * as THREE from 'three';
 
-const lenis = new Lenis()
-
-lenis.on('scroll', (e) => {
-})
-
-lenis.on('scroll', ScrollTrigger.update)
-
-gsap.ticker.add((time)=>{
-  lenis.raf(time * 1000)
-})
-
-gsap.ticker.lagSmoothing(0)
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
